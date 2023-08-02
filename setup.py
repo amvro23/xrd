74cf3756039f95bd0fe25c6cdb90a53408487007
Amvrosios G. Georgiadis <91277572+amvro23@users.noreply.github.com>
2023-08-02 17:53:15 +0300

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
    name='xrd',
-   version='0.1.0.rc3',
+   version='0.1.0.rc2',
    description='XRD package.',
    long_description=long_description,
    author='Amvrosios Georgiadis',
@@ -15,9 +15,8 @@ setup(
    packages=['xrd'],
    install_requires=[
       'numpy==1.19.*',
-      'pandas>=1.1.*',
       'scipy>=1.7.*',
+      'pandas>=1.1.*',
       'matplotlib',
-      'lmfit',
       ],
 )
